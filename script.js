5a816e4aec40607251eec45875b18afa3fa25cd0
Zach <shaw.zacht@gmail.com>
2022-02-14 17:06:55 -0800

    add html sections, js click alert

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "I'm getting this shit figured out";
 
 document.querySelector("body").appendChild(h2);
+
+const face = document.getElementById('citeTitle');
+
+function clickAlert() {
+    alert('Face image coming soon')
+}
+
+face.addEventListener('click', clickAlert);
